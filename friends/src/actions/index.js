4c63189c1f5465d4cdcb0fa4864164d6b6aceea6
Kayla <6lackmacaroon@gmail.com>
2019-05-16 14:17:18 -0600

    built action creators

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -1,18 +1,35 @@
 import axios from 'axios';
-
+import { axiosWithAuth } from '../axiosWithAuth';
+//action types
+export const LOGIN_START = 'LOGIN_START';
+export const LOGIN_SUCCEED = 'LOGIN_SUCCEED';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const FETCH_FRIENDS = 'FETCH_FRIENDS';
-export const SUCCESS = 'SUCCESS';
-export const FAILURE = 'FAILURE';
+export const FETCH_SUCCESS = 'SUCCESS';
+export const FETCH_FAIL = 'FAILURE';
 
-export const getFriends = () => dispatch => {
-      dispatch({ type: FETCH_FRIENDS });
-      axios
-            .get('http://localhost:5000/api/friends/')
+//action creators
+export const login = () => dispatch => {
+      dispatch({ type: LOGIN_START });
+      return axios
+            .post('http://localhost:5000/api/login', creds)
             .then(res => {
-                  dispatch({ type: SUCCESS, payload: res.data });
+                  localStorage.setItem('token', res.data.payload)
+                  dispatch({ type: LOGIN_SUCCESS, payload: res.data.payload });
             })
             .catch(err => {
                   console.log('action error', err);
-                  dispatch({ type: FAILURE, payload: err.data });
+                  dispatch({ type: LOGIN_FAIL, payload: err.data });
             })
 }
+export const getFriends = () => dispatch => {
+      dispatch({ type: FETCH_FRIENDS });
+      axiosWithAuth()
+            .get('/api/friends')
+            .then( res => {
+                  dispatch({ type: FETCH_SUCCESS, payload: res.data.data })
+            })
+            .catch(err => {
+                  dispatch({ type: FETCH_FAIL, payload: err.data})
+            })
+} 
