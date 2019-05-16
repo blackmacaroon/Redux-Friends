c213a7dea33b4444a54037386a80e0de188b8b00
Kayla <6lackmacaroon@gmail.com>
2019-05-16 14:04:21 -0600

    built app.js with links and routes

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { FriendListView } from './views/FriendListView';
 import './App.css';
 
 function App() {
   return (
-    <FriendListView />
+    <Router>
+      <div className='App'>
+        <ul>
+          <li>
+            <Link to='/api/login'>Login</Link>
+          </li>
+          <li>
+            <Link to='/api/friends'>Private Friend List</Link>
+          </li>
+        </ul>
+        <Route path='/api/login' component={Login} />
+        <PrivateRoute exact path='/api/friends' component={FriendListView} />
+      </div>
+    </Router>
   );
 }
 
